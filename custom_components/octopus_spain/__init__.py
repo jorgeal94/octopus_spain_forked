bfff5ae3c524cb1c16fa221e4d08529254fb0d9b
Jorge Alonso Lopez <xurxo.1@hotmail.com>
2025-03-13 16:31:20 +0100

    Update __init__.py

--- custom_components/octopus_spain/__init__.py
+++ custom_components/octopus_spain/__init__.py
@@ -7,7 +7,7 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-PLATFORMS: list[Platform] = [Platform.SENSOR,Platform.SELECT]
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
