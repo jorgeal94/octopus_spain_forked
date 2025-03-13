f5c0854666169530b935a9fabe214fc04bc06225
Jorge Alonso Lopez <xurxo.1@hotmail.com>
2025-03-13 16:26:31 +0000

    select

--- custom_components/octopus_spain/const.py
+++ custom_components/octopus_spain/const.py
@@ -5,4 +5,7 @@ DOMAIN = "octopus_spain"
 CONF_EMAIL = 'email'
 CONF_PASSWORD = 'password'
 
-UPDATE_INTERVAL = 2  # Hours
+UPDATE_INTERVAL = 1 # Hours
+
+INTELLIGENT_CHARGE_TIMES: Final = [f"{hour:02}:{mins:02}" for hour in range(4, 12) for mins in INTELLIGENT_MINS_PAST_HOURS][:-1]
+INTELLIGENT_SOC_OPTIONS: Final = list(range(10, 105, 5))
